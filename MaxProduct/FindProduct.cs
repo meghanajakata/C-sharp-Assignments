8e9c1254ee226a88e3243972e5f8bae40ba16316
meghanajakata <jakatameghana@gmail.com>
2023-05-24 12:32:46 +0530

    first assignment

---
+++ MaxProduct/FindProduct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MaximumFourDigitProduct
+{
+    public class FindProduct
+    {
+        public long max_pdt = 1;
+        public string num;
+        int left = 0, right = 4;
+
+        public int CalculateProduct(string number)
+        {
+            return number.Length();
+            /*
+            int currentProduct = 1;
+            int value;
+            int maximumProduct = 1;
+            int left = 0, right = 4;
+
+            for (int i = 0; i < 4; i++)
+            {
+                value = Convert.ToInt32(num[i]) - 48;
+                maximumProduct = maximumProduct * value;
+            }
+            currentProduct = maximumProduct;
+
+            while (right < number.Length && left < right)
+            {
+                value = Convert.ToInt32(num[left++] - 48);
+                currentProduct = currentProduct / value;
+                value = Convert.ToInt32(num[right++] - 48);
+                currentProduct = currentProduct * value;
+                maximumProduct = Math.Max(maximumProduct, currentProduct);
+            }*/
+
+        }
+    }
+}
