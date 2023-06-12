e897e58cfb4f2e9f153775a727fd4cd9736137a2
meghanajakata <jakatameghana@gmail.com>
2023-06-12 20:11:31 +0530

    Three-Tier-Example

---
+++ ThreeTierExample/ThreeTierExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+
+namespace ThreeTierExample
+{
+    public class Program
+    {
+        public int add()
+        {
+            Debug.WriteLine("in program");
+            return 1;
+        }
+    }
+}
---
+++ ThreeTierExample/ThreeTierExample/default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ThreeTierExample
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void BtnSave_Click(object sender, EventArgs e)
+        {
+            
+            //Program p = new Program();
+            //Console.WriteLine(p.add());
+
+        }
+    }
+}
